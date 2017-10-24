748bd9880ca7951e7da196b3537575057efb010c
Jan Jirout <jirout@gmail.com>
2017-10-24 13:24:16 +0200

    simple spring boot app

---
+++ example-2/src/main/java/skoleni2/HelloController.java
@@ -0,0 +1,24 @@
+package skoleni2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+    	StringBuilder buff = new StringBuilder(100);
+    	buff.append("<html>");
+    	buff.append("<head>");
+    	buff.append("</head>");
+    	buff.append("<body>");
+    	buff.append("<h1>Ahoj lidi!</h1>");
+    	buff.append("");
+    	buff.append("");
+    	buff.append("</body>");
+    	buff.append("</html>");
+        return buff.toString();
+    }
+
+}
